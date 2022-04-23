f4f599025cfd24b3352f732d8aa64a0abbc176ec
RunzhouHan <rzhan@bu.edu>
2022-04-23 15:46:11 -0500

    corrected a typo in example rdf

--- user_engine/visualizer/visualizer.py
+++ user_engine/visualizer/visualizer.py
@@ -1,1 +1,25 @@
-# from provio.Extensible import Extensible
+# import rdflib
+# from rdflib.extras.external_graph_libs import rdflib_to_networkx_multidigraph
+# import networkx as nx
+# import matplotlib.pyplot as plt
+
+# _rdf_path = '/home/runzhou/Downloads/prov-io/c/doc/example_prov.ttl'
+
+# g = rdflib.Graph()
+# result = g.parse(_rdf_path, format='turtle')
+
+# G = rdflib_to_networkx_multidigraph(result)
+
+# # Plot Networkx instance of RDF Graph
+# pos = nx.spring_layout(G, scale=2)
+# edge_labels = nx.get_edge_attributes(G, 'r')
+# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+# nx.draw(G, with_labels=True)
+
+# #if not in interactive mode for 
+# plt.show()
+
+import requests
+
+# Replace with your provenance file or uri
+response = requests.get("http://www.ldf.fi/service/rdf-grapher?rdf=example_prov.ttl&from=ttl&to=png")
